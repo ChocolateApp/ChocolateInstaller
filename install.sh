3215993762507cfb510bf9f3888ff7b669268f9b
impr.visible@gmail.com <impr.visible@gmail.com>
2023-04-17 10:06:24 +0200

    installer update
    New installer and update for linux

--- install.sh
+++ install.sh
@@ -7,24 +7,87 @@ if [ $(id -u) -ne 0 ]; then
 fi
 echo "Hi, thanks for installing Chocolate, I'm your guide for this installation."
 
-pythonRequirement="https://raw.githubusercontent.com/Impre-visible/Chocolate/main/requirements.txt"
-
-if ! [ -x "$(command -v python3)" ]; then
-    apt install -y python3
-    echo "I installed python3"
+if command -v python &>/dev/null; then
+    python_version=$(python -c 'import sys; print("".join(map(str, sys.version_info[:3])))')
+    python_version=$((python_version + 0))
+    echo $python_version
+    if [ $(echo "$python_version >= 390" | bc -l) -eq 1]; then
+        echo "Python 3.9 or higher is installed"
+    else
+        echo "Python 3.9 or higher is required. Please install it and try again."
+        exit 1
+    fi
+elif command -v python3 &>/dev/null; then
+    python_version=$(python3 -c 'import sys; print("".join(map(str, sys.version_info[:3])))')
+    if [ $(echo "$python_version >= 390" | bc -l) -eq 1 ]; then
+        echo "Python 3.9 or higher is installed"
+    else
+        echo "Python 3.9 or higher is required. Please install it and try again."
+        exit 1
+    fi
+else
+    echo "Python is not installed. Installing Python 3.10..."
+    sudo apt-get update && sudo apt-get install python3.10 -y
 fi
-if ! [ -x "$(command -v pip3)" ]; then
-    apt install -y python3-pip
-    echo "I installed pip3"
-    sudo apt-get install -y crudini
-fi
-pip3 install -r "$pythonRequirement" --quiet --exists-action i
-chocolateRepo="https://github.com/ChocolateApp/Chocolate.git"
-linuxStart="https://raw.githubusercontent.com/ChocolateApp/ChocolateInstallLinux/main/start.sh"
-git clone "$chocolateRepo" Chocolate/ --quiet
-echo "start.sh" >> Chocolate/.gitignore
-echo "database.db" >> Chocolate/.gitignore
 
+# Vérifier si pip3 est installé
+if command -v pip3 &>/dev/null; then
+    echo "pip3 is installed"
+else
+    echo "pip3 is not installed. Installing pip3..."
+    sudo apt-get update && sudo apt-get install python3-pip -y
+fi
+
+# Vérifier si node est installé
+if command -v node &>/dev/null; then
+    echo "node is installed"
+else
+    echo "node is not installed. Installing node..."
+    sudo apt-get update && sudo apt-get install nodejs -y
+fi
+
+# Vérifier si npm est installé
+if command -v npm &>/dev/null; then
+    echo "npm is installed"
+else
+    echo "npm is not installed. Installing npm..."
+    sudo apt-get update && sudo apt-get install npm -y
+fi
+
+# Vérifier si ffmpeg est installé
+if command -v ffmpeg &>/dev/null; then
+    echo "ffmpeg is installed"
+else
+    echo "ffmpeg is not installed. Installing ffmpeg..."
+    sudo apt-get update && sudo apt-get install ffmpeg -y
+fi
+
+# Vérifier si crudini est installé
+if command -v crudini &>/dev/null; then
+    echo "crudini is installed"
+else
+    echo "crudini is not installed. Installing crudini..."
+    sudo apt-get update && sudo apt-get install crudini -y
+fi
+
+# Cloner les dépôts GitHub
+sudo mkdir -p /etc/chocolate/back /etc/chocolate/front
+sudo git clone https://github.com/ChocolateApp/Chocolate.git /etc/chocolate/back
+sudo git clone https://github.com/ChocolateApp/ChocolateReact.git /etc/chocolate/front
+
+cd /etc/chocolate/front
+sudo npm install --silent
+
+cd /etc/chocolate/back
+sudo pip install -r requirements.txt
+
+# Copier le fichier start.sh
+sudo curl -o /etc/chocolate/start.sh https://raw.githubusercontent.com/ChocolateApp/ChocolateInstallLinux/main/start.sh
+
+# Ajouter la commande chocolate comme alias
+sudo echo 'alias chocolate="sudo /etc/chocolate/start.sh"' >> ~/.bashrc
+
+# Ajouter les variables de configuration pour TMDB et la langue par défaut
 echo "We need to set up the default settings"
 echo ""
 echo "Let's start by creating a TMDB API key, it's very important, so please follow this link and the instructions to get one: https://developers.themoviedb.org/3/getting-started/introduction"
@@ -43,7 +106,7 @@ mv Chocolate /etc/chocolate 2>/dev/null
 wget -q "$linuxStart" -O /etc/chocolate/start.sh
 chmod +x /etc/chocolate/start.sh
 
-echo "alias chocolate='/etc/chocolate/start.sh'" >> ~/.bashrc
+echo "alias chocolate='sudo sh /etc/chocolate/start.sh'" >> ~/.bashrc
 
 echo "Awesome, Chocolate is installed to /etc/chocolate and is now ready to use!"
 echo "You can start Chocolate by typing 'chocolate' in the terminal. You can edit the settings directly on the settings page in Chocolate."
--- start.sh
+++ start.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 
-version=$(crudini --get /etc/chocolate/config.ini ChocolateSettings version)
-curl -s -L https://raw.githubusercontent.com/ChocolateApp/Chocolate/main/config.ini > /etc/chocolate/configTemp.ini
-latestVersion=$(crudini --get /etc/chocolate/configTemp.ini ChocolateSettings version)
-rm /etc/chocolate/configTemp.ini
-version=$(echo $version | tr -d '.')
-latestVersion=$(echo $latestVersion | tr -d '.')
-version=$((version))
-latestVersion=$((latestVersion))
-if [ $version == $latestVersion ]; then
-    python3 /etc/chocolate/app.py
-    exit 0
-elif [ $latestVersion > $version ]; then
-    echo "You are running an old version of Chocolate"
-    read -p "Do you want to update? [y/n] " updating
-    echo
-    if [[ $updating =~ ^[Yy]$ ]]; then
-        echo "Updating..."
-        sudo wget -q -O install.sh 'https://raw.githubusercontent.com/ChocolateApp/ChocolateInstallLinux/main/install.sh'
-        #clear all /etc/chocolate files except config.ini, database.db
-        rm -rf /etc/chocolate/static
-        rm -rf /etc/chocolate/templates
-        rm -rf /etc/chocolate/app.py
-        rm -rf /etc/chocolate/start.sh
-        rm -rf /etc/chocolate/.gitignore
-        rm -rf /etc/chocolate/rpc.py
-        rm -rf /etc/chocolate/.git
-        rm -rf /etc/chocolate/requirements.txt
-        bash /install.sh
-        rm install.sh
-    fi
-fi
-exit 0
+cd /etc/chocolate/front
+sudo npm start &
+cd /etc/chocolate/back
+sudo python app.py &
